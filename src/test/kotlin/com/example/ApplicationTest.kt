ad96ff80b528996928a76beee5e326756662e4aa
Renato Viana <renatoviana30@gmail.com>
2022-04-30 20:39:39 -0300

    Testing All Heroes Endpoint - Part 1

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -1,14 +1,25 @@
 package com.example
 
+import com.example.models.ApiResponse
+import com.example.repository.HeroRepository
+import com.example.repository.NEXT_PAGE_KEY
+import com.example.repository.PREVIOUS_PAGE_KEY
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import org.koin.java.KoinJavaComponent.inject
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ApplicationTest {
+
+    private val heroRepository: HeroRepository by inject(HeroRepository::class.java)
+
     @Test
-    fun testRoot() {
+    fun `access root endpoint, assert correct information`() {
         withTestApplication(moduleFunction = Application::module) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(
@@ -18,6 +29,97 @@ class ApplicationTest {
                 assertEquals(
                     expected = "Welcome to Boruto API",
                     actual = response.content
+                )
+            }
+        }
+    }
+
+    @ExperimentalSerializationApi
+    @Test
+    fun `access all heroes endpoint, query all pages, assert correct information`() {
+        withTestApplication(moduleFunction = Application::module) {
+            val pages = 1..5
+            val heroes = listOf(
+                heroRepository.page1,
+                heroRepository.page2,
+                heroRepository.page3,
+                heroRepository.page4,
+                heroRepository.page5
+            )
+
+            pages.forEach { page ->
+                println("CURRENT PAGE: $page")
+                handleRequest(HttpMethod.Get, "/boruto/heroes?page=$page").apply {
+                    assertEquals(
+                        expected = HttpStatusCode.OK,
+                        actual = response.status()
+                    )
+
+                    val expected = ApiResponse(
+                        success = true,
+                        message = "ok",
+                        prevPage = caculatePage(page = page)["prevPage"],
+                        nextPage = caculatePage(page = page)["nextPage"],
+                        heroes = heroes[page - 1]
+                    )
+
+                    val actual = Json.decodeFromString<ApiResponse>(response.content.toString())
+                    println("PREV PAGE: ${caculatePage(page = page)["prevPage"]}")
+                    println("NEXT PAGE: ${caculatePage(page = page)["nextPage"]}")
+                    println("HEROES: ${heroes[page - 1]}")
+                    assertEquals(
+                        expected = expected,
+                        actual = actual
+                    )
+                }
+            }
+
+        }
+    }
+
+    private fun caculatePage(page: Int): Map<String, Int?> {
+        var prevPage: Int? = page
+        var nextPage: Int? = page
+
+        if (page in 1..4) {
+            nextPage = nextPage?.plus(1)
+        }
+        if (page in 2..5) {
+            prevPage = prevPage?.minus(1)
+        }
+        if (page == 1) {
+            prevPage = null
+        }
+        if (page == 5) {
+            nextPage = null
+        }
+
+        return mapOf(PREVIOUS_PAGE_KEY to prevPage, NEXT_PAGE_KEY to nextPage)
+    }
+
+    @ExperimentalSerializationApi
+    @Test
+    fun `access all heroes endpoint, query second page, assert correct information`() {
+        withTestApplication(moduleFunction = Application::module) {
+            handleRequest(HttpMethod.Get, "/boruto/heroes?page=2").apply {
+                assertEquals(
+                    expected = HttpStatusCode.OK,
+                    actual = response.status()
+                )
+                val expected = ApiResponse(
+                    success = true,
+                    message = "ok",
+                    prevPage = 1,
+                    nextPage = 3,
+                    heroes = heroRepository.page2
+                )
+
+                val actual = Json.decodeFromString<ApiResponse>(response.content.toString())
+                println("EXPECTED: $expected")
+                println("ACTUAL: $actual")
+                assertEquals(
+                    expected = expected,
+                    actual = actual
                 )
             }
         }
